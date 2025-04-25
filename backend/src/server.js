50ae41f26a62b12ce5c5447b818a125777a37ea0
Darkpwd <natanyuri1010@gmail.com>
2025-04-25 17:32:54 -0300

    build(api-docs): integrate Swagger UI and set up base routes documentation

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,40 +1,50 @@
-import express, { json } from "express";
+import express from "express";
 import colors from "colors";
 import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import { router as routerAuth } from "./routes/authRoutes.js";
+import path from "path";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
+
+import { router as authRoutes } from "./routes/authRoutes.js";
 import { router as userRoutes } from "./routes/userRoutes.js";
-import { router } from "./routes/testingRoutes.js";
+import { router as testingRoutes } from "./routes/testingRoutes.js";
 import { connectDB } from "./config/db.js";
 
-// dot env config
+// Config .env
 dotenv.config();
 
-//DB connection
+// Swagger config
+const swaggerFile = YAML.load(path.resolve("src/docs/swagger.yaml"));
+
+// Connect to DB
 connectDB();
 
+// App initialization
 const app = express();
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use("/api/v1/test", router); // path from my address web
-app.use("/api/v1/auth", routerAuth); // route auth
-app.use("/api/v1/user", userRoutes); // user route
 
-// Create my routes
-// get route
+// Swagger docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+// Routes
+app.use("/api/v1/test", testingRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+
+// Default route
 app.get("/", (req, res) => {
-  return res.status(200).send("<h1>Hello World!</h1>");
+  res.status(200).send("<h1>Hello World!</h1>");
 });
 
-// PORT
+// Server
 const PORT = process.env.PORT || 8088;
 
-// Create a server for connection
 app.listen(PORT, () => {
-  // Define the color from my log message
-  console.log(`Server Running in ${PORT} port.`.white.bgMagenta);
+  console.log(`🚀 Server running on port ${PORT}`.white.bgMagenta);
 });
